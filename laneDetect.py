09d4a4ce0a3b8f431e397bf2e5a9ad9c5dfb9c77
Satyam Yadav <satyamyadav13579@gmail.com>
2017-01-11 22:42:49 +0530

    changes in params for roi and line votes

--- laneDetect.py
+++ laneDetect.py
@@ -42,8 +42,8 @@ def leftIntercept(x1,y1,x2,y2,frame,i,j):
     print "Left:",p.x," ",p.y
     cv2.imshow('Left',frame)
     return p.y'''
-    
-cap = cv2.VideoCapture('vid1.mp4')
+
+cap = cv2.VideoCapture('Highway to Pakistan, from India, on NH1!.mp4')
 
 while(True):
     ret, frame = cap.read()
@@ -96,70 +96,71 @@ while(True):
         
                 radian = theta
                 theta = degrees(theta)
-                if (theta >= 10 and theta <= 70):    
+                if (theta >= 30 and theta <= 70):    
                     rads.append(radian)
-                if (theta >=90 and theta <=160 ):                                 
+                if (theta >=100 and theta <=140 ):                                 
                     rads.append(radian)
-    
     try:
-        if(len(rads) > 0 ):
-            minr = min(rads)
-            for index in range(len(lines)):
-                #print "s"
-                for rho,theta in lines[index]:
-                    
-                    if max(rads) == theta:
-                        maxr = max(rads)
-                        x1,y1,x2,y2 = calcX1Y1X2Y2(max(rads))
-                        cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),3)
-                        line1 = LineString([(x1,y1),(x2,y2)])
-                        line2 = LineString([(0,i), (j,i)])
-                        p = (line1.intersection(line2)) 
-                        if not line1.intersection(line2):
-                            line2 = LineString([(j,0), (j, i)])
-                            p = (line1.intersection(line2)) 
-                        cv2.circle(frame,(int(p.x), int(p.y)),10,(0,255,0),3)
-                        #print "Right:",p.y
-                        #cv2.imshow('Right',frame)
-                        Y2 = p.y
-                        print Y2
-                        
-                    if min(rads) == theta:                                    
-                        minr = min(rads)
-                        x1,y1,x2,y2 = calcX1Y1X2Y2(min(rads))
-                        cv2.line(frame,(x1,y1),(x2,y2), (255, 0,0),3)    
-                        line1 = LineString([(x1,y1),(x2,y2)])
-                        line2 = LineString([(0,i), (j,i)])
-                        p = (line1.intersection(line2))
-                        if not line1.intersection(line2):
-                            line2 = LineString([(0,0), (0, i)])
-                            p = (line1.intersection(line2))
-                            
-                        cv2.circle(frame,(int(p.x), int(p.y)),10,(255,0,0),3)
-                        #print "Left:",p.y
-                        #cv2.imshow('Left',frame)
-                        Y1 = p.y
-                        print Y1
-        
-                    d = Y1-Y2         
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-                    a=j/10
-                    b=9*i/10
-                    if abs(d) >= i/10:
-                        if d>0:
-                            cv2.putText(frame,"Shift Right.", (a,b),font,1,(255,255,0),2,cv2.LINE_AA)
-                        else:
-                            cv2.putText(frame,"Shift Left.", (a,b),font,1,(255,255,0),2,cv2.LINE_AA)
-                    else:
-                        cv2.putText(frame,"You are on the right track.", (a,b),font,1,(255,255,0),2,cv2.LINE_AA)
-                
-        print lines
-        #print rads
-        cv2.imshow('Lane Markers',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    
+	    if(len(rads) > 0 ):
+		minr = min(rads)
+		for index in range(len(lines)):
+		    #print "s"
+		    for rho,theta in lines[index]:
+		        
+		        if max(rads) == theta:
+		            maxr = max(rads)
+		            x1,y1,x2,y2 = calcX1Y1X2Y2(max(rads))
+		            cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),3)
+		            line1 = LineString([(x1,y1),(x2,y2)])
+		            line2 = LineString([(0,i), (j,i)])
+		            p = (line1.intersection(line2)) 
+		            if not line1.intersection(line2):
+		                line2 = LineString([(j,0), (j, i)])
+		                p = (line1.intersection(line2)) 
+		            cv2.circle(frame,(int(p.x), int(p.y)),10,(0,255,0),3)
+		            #print "Right:",p.y
+		            #cv2.imshow('Right',frame)
+		            Y2 = p.y
+		            print Y2
+		            
+		        if min(rads) == theta:                                    
+		            minr = min(rads)
+		            x1,y1,x2,y2 = calcX1Y1X2Y2(min(rads))
+		            cv2.line(frame,(x1,y1),(x2,y2), (255, 0,0),3)    
+		            line1 = LineString([(x1,y1),(x2,y2)])
+		            line2 = LineString([(0,i), (j,i)])
+		            p = (line1.intersection(line2))
+		            if not line1.intersection(line2):
+		                line2 = LineString([(0,0), (0, i)])
+		                p = (line1.intersection(line2))
+		                
+		            cv2.circle(frame,(int(p.x), int(p.y)),10,(255,0,0),3)
+		            #print "Left:",p.y
+		            #cv2.imshow('Left',frame)
+		            Y1 = p.y
+		            print Y1
+	    
+		        d = Y1-Y2         
+		        font = cv2.FONT_HERSHEY_SIMPLEX
+		        a=j/10
+		        b=9*i/10
+		        if abs(d) >= i/10:
+		            if d>0:
+		                cv2.putText(frame,"Shift Right.", (a,b),font,1,(255,255,0),2,cv2.LINE_AA)
+		            else:
+		                cv2.putText(frame,"Shift Left.", (a,b),font,1,(255,255,0),2,cv2.LINE_AA)
+		        else:
+		            cv2.putText(frame,"You are on the right track.", (a,b),font,1,(255,255,0),2,cv2.LINE_AA)
+		    
+	    print lines
+	    #print rads
+	    cv2.imshow('Lane Markers',frame)
+	    if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
     except:
-        continue        
+		continue
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
